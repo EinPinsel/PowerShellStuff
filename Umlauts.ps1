9c1b76d4c2f538194b9c14fc2fa96bc76be6ead6
EinPinsel <cobhc939@gmx.de>
2016-02-18 22:05:09 +0100

    First try. Needs work. Again :D

---
+++ Umlauts.ps1
@@ -0,0 +1,16 @@
+﻿# Find folder on NAS
+$drive = '\\freenas\restore\Bilder'
+
+
+if (Test-Path -path $drive)
+{
+  # To make the handling a little bit more consistent, create a PSDrive.
+  New-PSDrive -PSProvider FileSystem -Name 'FreeNASBilder' -Root $drive
+  Set-Location FreeNASBilder:\
+  Get-ChildItem -Recurse -Force | Where-Object { $_.Name -like '*ä*' } | Rename-Item -NewName { $_.Name -replace 'ä','ae' }
+  Get-ChildItem -Recurse -Force | Where-Object { $_.Name -like '*ö*' } | Rename-Item -NewName { $_.Name -replace 'ö','oe' }
+  Get-ChildItem -Recurse -Force | Where-Object { $_.Name -like '*ü*' } | Rename-Item -NewName { $_.Name -replace 'ü','ue' }
+  Get-ChildItem -Recurse -Force | Where-Object { $_.Name -like '*ß*' } | Rename-Item -NewName { $_.Name -replace 'ß','ss'}
+  
+    Remove-PSDrive -Name 'FreeNASBilder'
+}
