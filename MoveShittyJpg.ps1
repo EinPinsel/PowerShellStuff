6c05a2d30e3345e00ded836f81831563ae2eb1b1
EinPinsel <cobhc939@gmx.de>
2016-02-18 19:40:29 +0100

    Needs some work
    because now it moves all jpgs, not only the ones with corresponding
    cr2-files

---
+++ MoveShittyJpg.ps1
@@ -0,0 +1,19 @@
+﻿function Move-ShittyJpg
+{
+    <#
+        .SYNOPSIS
+        Short Description
+        .DESCRIPTION
+        Detailed Description
+        .EXAMPLE
+        Move-ShittyJpeg
+        explains how to use the command
+        can be multiple lines
+        .EXAMPLE
+        Move-ShittyJpeg
+        another example
+        can have as many examples as you like
+    #>
+    Get-ChildItem *.jpg | % {if(Test-Path ($_.fullname.replace('.jpg','cr2'))) {Move-Item $_ .\halde\}}
+}
+
